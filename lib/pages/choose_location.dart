3a6159f4b0a8eaf5bedbdc5176e8e6e4e1e7591c
Takeyuki UEDA <ueda@latelierdueda.com>
2019-10-07 20:43:16 +0900

    lesson #25

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,6 +6,21 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  void getData() async {
+    // simulate network request for usermame
+    Future.delayed(Duration(seconds: 3), () {
+      print('yoshi');
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
